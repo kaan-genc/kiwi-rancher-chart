6b8dcd87a2ede7f9f00680db3451d685a54e485b
Kaan Genc <kaan.genc@tani.com.tr>
2026-04-08 22:57:24 +0300

    update

---
+++ images/postgres_settings.py
@@ -0,0 +1,13 @@
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("KIWI_DB_NAME", "kiwitcms"),
+        "USER": os.environ.get("KIWI_DB_USER", "kiwi"),
+        "PASSWORD": os.environ.get("KIWI_DB_PASSWORD", ""),
+        "HOST": os.environ.get("KIWI_DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("KIWI_DB_PORT", "5432"),
+        "CONN_MAX_AGE": 60,
+    }
+}
