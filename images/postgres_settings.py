3906e01a9cc1d3408b6e856bfbbcb7694c06ac11
kaan-genc <kaan.genc@chippin.com.tr>
2026-04-08 23:33:01 +0300

    Update postgres_settings.py

--- images/postgres_settings.py
+++ images/postgres_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
         "NAME": os.environ.get("KIWI_DB_NAME", "kiwitcms"),
         "USER": os.environ.get("KIWI_DB_USER", "kiwi"),
         "PASSWORD": os.environ.get("KIWI_DB_PASSWORD", ""),
-        "HOST": os.environ.get("KIWI_DB_HOST", "127.0.0.1"),
+        "HOST": os.environ.get("KIWI_DB_HOST", "10.190.205.6"),
         "PORT": os.environ.get("KIWI_DB_PORT", "5432"),
         "CONN_MAX_AGE": 60,
     }
